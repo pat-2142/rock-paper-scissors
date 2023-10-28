c9da9ea871151bb1ba16fa376704f23390e0f3e5
LORD PYRHUS <phatsimopheko11@gmail.com>
2023-10-28 18:35:29 +0200

    Completed single round logic

--- scripts.js
+++ scripts.js
@@ -1,7 +1,34 @@
 let choiceArray = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    return choiceArray[Math.floor(Math.random()*choiceArray.length)];
+    return choiceArray[Math.floor(Math.random() * choiceArray.length)];
 }
 
-console.log(getComputerChoice(choiceArray));
+const playerSelection = "rock";
+const computerSelection = getComputerChoice(choiceArray);
+
+console.log("Computer Choice: " + computerSelection);
+console.log("Player Choice: " + playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    const rules = {
+        rock: 'scissors',
+        paper: 'rock',
+        scissors: 'paper'
+    };
+
+    if (computerSelection === playerSelection) {
+        console.log("It's a tie! Both chose " + computerSelection + ".");
+        return 'tie';
+    }
+
+    if (rules[playerSelection] === computerSelection) {
+        console.log('Player wins! ' + playerSelection + ' beats ' + computerSelection + '.');
+        return 'player';
+    }
+
+    console.log('Computer wins! ' + computerSelection + ' beats ' + playerSelection + '.');
+    return 'computer';
+}
+
+playRound(playerSelection, computerSelection);
